12583207cea5bbc44b380f17899968c1d8266cc2
bootstraponline <code@bootstraponline.com>
2013-06-05 21:35:59 -0400

    Remove unused code

--- blank/src/main/java/com/bootstraponline/blank/MainActivity.java
+++ blank/src/main/java/com/bootstraponline/blank/MainActivity.java
@@ -1,28 +1,20 @@
 package com.bootstraponline.blank;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.WallpaperManager;
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
-import android.service.wallpaper.WallpaperService;
 import android.view.Display;
-import android.view.Menu;
-import android.view.WindowManager;
 
 import static android.graphics.Bitmap.createBitmap;
 
-@TargetApi(17) // required so Android Studio's Lint doesn't complain
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        super.onCreate(savedInstanceState); // must call super.onCreate
         final WallpaperManager wall = WallpaperManager.getInstance(getApplicationContext());
         final Display display = getWindowManager().getDefaultDisplay();
 
@@ -40,12 +32,4 @@ public class MainActivity extends Activity {
             finish(); // we're done
         }
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
 }
